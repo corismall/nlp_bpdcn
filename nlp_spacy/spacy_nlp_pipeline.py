700e635a933191f8fd79147591d14e04eef3ffad
Corinn Small <corinn.small@gmail.com>
2021-05-07 18:03:42 -0700

    use matcher to create training data to train own model

--- nlp_spacy/spacy_nlp_pipeline.py
+++ nlp_spacy/spacy_nlp_pipeline.py
@@ -1,13 +1,15 @@
 import spacy
 from spacy import displacy
 from spacy.matcher import Matcher
-from spacy.tokens import Token
+from spacy.tokens import Doc, Token, Span
 
 # Load the large English NLP model
 nlp = spacy.load('en_core_web_lg')
 matcher = Matcher(nlp.vocab)
 
-# The text we want to examine
+# The text we want to examine from PDF_PAGE_FILE
+
+#file = PDF_PAGE_FILE
 
 def get_text(file):
     '''opens text file and returns text'''
@@ -16,15 +18,23 @@ def get_text(file):
         text = f.read().replace('\n', '')
         return text
 
-# Add match ID "cd marker negative" with no callback and one pattern
-pos_exp = ['+']
-neg_exp = ['-']
+#def get_doi(FILE_RESPONSE.JSON):
+    #doi = something
+    #return doi
 
-pos_pattern = [{"IS_PUNCT": True}, {"TEXT": '+'}, {"IS_PUNCT": True}]
-neg_pattern = [{"IS_PUNCT": True}, {"TEXT": '-'}, {"IS_PUNCT": True}]
+#def get_page(PDF_PAGE_FILE)
+    #page = something
+    #return page
 
-#matcher.add('cd positive', [pos_pattern])
-#matcher.add('cd negative', [neg_pattern])
+#for later:
+# Doc.set_extension('id', default=None)
+# Doc.set_extension('page_number', default=None)
+#
+# data = list(get_text(file), {"id": doi, "page_number: page"})
+
+'''for doc, context in nlp.pipe(data, as_tuples=True):
+    doc._.id = context["id"]
+    doc._.page_number = context["page_number"]'''
 
 
 # Parse the text with spaCy. This runs the entire pipeline.
@@ -43,38 +53,50 @@ by cytogenetic analysis of the bone marrow (46,XX).'''
 clean_text = " ".join(text.split())
 #print(text, clean_text)
 
-#spacy tokenize
+#create doc
 
 doc = nlp(clean_text)
+ 
+
+# Use matcher to create training data!
+# Add match ID "cd marker negative" with no callback and one pattern
+pos_exp = ['+']
+neg_exp = ['-']
+
+pos_pattern = [{"IS_PUNCT": True}, {"TEXT": '+'}, {"IS_PUNCT": True}]
+neg_pattern = [{"IS_PUNCT": True}, {"TEXT": '-'}, {"IS_PUNCT": True}]
+
+matcher.add('cd positive', [pos_pattern])
+matcher.add('cd negative', [neg_pattern])
+
 
 matches = matcher(doc)
+#print(matches)
 
 for match_id, start, end in matches:
-    string_id = doc.vocab.strings[match_id]  # look up string id
-    span = doc[start:end]
-    print(string_id, span.text)
+    string_id = doc.vocab.strings[match_id]  # look up string hash id
+    span = Span(doc, start, end)
+    print(span)
 
-cd_exp = []
 
-for match_id, start, end in matches:
-    if doc.vocab.strings[match_id] == "cd positive" or "cd negative":
-        cd_exp.append(doc[start:end])
+print(Doc.doc.ents)
 
-Token.set_extension("is_expression", default=False)
 
-with doc.retokenize() as retokenizer:
-    for span in cd_exp:
-        retokenizer.merge(span)
-        for token in span:
-            token._.is_expression = True
+'''print(doc)
+print('')
+print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+print('')
+print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])'''
 
-for token in doc:
-    print(token)
-    if token.text == '(+)' or '(-)':
-        print('yes')
-    else:
-        pass    
-    print(token.text, token._.is_expression, token.pos_)
+
+
+#for token in doc:
+ #   print(token.text)
+  #  if token.text == '(+)' or '(-)':
+   #     print('yes')
+    #else:
+     #   pass    
+    #print(token.text, token._.is_expression, token.pos_)
 
 #sentence_spans = list(doc.sents)
 #print(sentence_spans)
@@ -90,3 +112,43 @@ for token in doc:
 # For example, this will print out all the named entities that were detected:
 #for entity in doc.ents:
 #    print(f"{entity.text} ({entity.label_})")
+
+
+#How training works:
+#1) initialize the model weights randomly with nlp.begin_training
+#2) predict labels (text category, or an entity span and its type) with a few examples with the current weights by calling nlp.update
+#3) compare prediction with true labels
+#4) Calculate how to change weights to improve predictions
+#5) update weights slightly
+#6) go back to step 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
